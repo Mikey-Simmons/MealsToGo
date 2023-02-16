34632fe73c84760ed3850860a7350d1cee8fb18f
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-02-16 15:05:39 -0800

    linting_stuff

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, StatusBar, Text, View, SafeAreaView } from "react-native";
+import React from "react";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text style={styles.myText}>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.myText}>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "white",
+    marginTop: StatusBar.currentHeight,
+
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    flexGrow: 1,
+  },
+  search: {
+    backgroundColor: "green",
+    flex: 0.05,
+  },
+  list: {
+    backgroundColor: "blue",
+    flex: 1,
+  },
+  myText: {
+    padding: 10,
+    color: "white",
   },
 });
