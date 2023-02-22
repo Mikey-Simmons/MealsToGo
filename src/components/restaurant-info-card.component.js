185a11fd4e28341862372749bc9cc14d9ef55028
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-02-22 12:33:19 -0800

    styledComponents

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -2,9 +2,24 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import styled from "styled-components/native";
+
+//styles
+const Title = styled.Text`
+padding: 16px;
+color: red;
+`;
+const RestaurantCard = styled(Card)`
+background-color: whitesmoke;
+`;
+const CardCover = styled(Card.Cover)`
+padding: 20px;
+background-color: whitesmoke
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Some Restaurant",
+    name = "Some Restaurant!",
     icon,
     photos = ["https://rb.gy/okc0wd"],
     address = "100 some rando street",
@@ -14,23 +29,11 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
   return (
     <>
-      <Card elevation={5} style={styles.card}>
-        <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
-        <Text style={styles.title}>{name}</Text>
-      </Card>
+      <RestaurantCard elevation={5}>
+        <CardCover key={name}  source={{ uri: photos[0] }} />
+        <Title>{name}</Title>
+      </RestaurantCard>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "white",
-  },
-  cover: {
-    padding: 20,
-    backgroundColor: "white",
-  },
-  title: {
-    padding: 16,
-  },
-});
