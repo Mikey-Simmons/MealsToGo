2f75a8f8f851830c2ed0364911d7b89c8814e37c
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-02-24 12:07:42 -0800

    theme

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/screens/restaurants.screen";
 import React from "react";
+import { ThemeProvider } from "styled-components/native";
+
+import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+        <ExpoStatusBar />
+      </ThemeProvider>
     </>
   );
 }
