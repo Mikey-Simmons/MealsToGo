310d234198096d1e3d80fca7894b549c7e908d03
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-02-24 12:01:50 -0800

    theme

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -1,20 +1,20 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+//import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 //styles
 const Title = styled.Text`
-padding: 16px;
-color: red;
+  padding: 16px;
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 const RestaurantCard = styled(Card)`
-background-color: whitesmoke;
+  background-color: whitesmoke;
 `;
 const CardCover = styled(Card.Cover)`
-padding: 20px;
-background-color: whitesmoke
+  padding: 20px;
+  background-color: whitesmoke;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -30,10 +30,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   return (
     <>
       <RestaurantCard elevation={5}>
-        <CardCover key={name}  source={{ uri: photos[0] }} />
+        <CardCover key={name} source={{ uri: photos[0] }} />
         <Title>{name}</Title>
       </RestaurantCard>
     </>
   );
 };
-
