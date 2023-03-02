5ba56218274bddc8e927b4f08f9cd9ba217123de
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-03-02 14:34:14 -0800

    theme_stuff

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Text } from "react-native";
 import { RestaurantsScreen } from "./src/screens/restaurants.screen";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return (
+      <>
+        <Text>Loading</Text>
+      </>
+    );
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -6,8 +6,16 @@ import styled from "styled-components/native";
 
 //styles
 const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
+  
   color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body}
+`;
+const Address = styled.Text`
+  
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption}
 `;
 const RestaurantCard = styled(Card)`
   background-color: ${(props)=> props.theme.colors.bg.primary};
@@ -16,12 +24,15 @@ const CardCover = styled(Card.Cover)`
   padding: ${(props)=> props.theme.space[3]};
   background-color: ${(props)=> props.theme.colors.bg.primary};
 `;
+const Info = styled.View`
+  padding: ${(props)=> props.theme.space[3]};
+`
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant!",
     icon,
-    photos = ["https://rb.gy/okc0wd"],
+    photos = ["https://tb-static.uber.com/prod/image-proc/processed_images/3e2e785f12c20cc3fc37435b2999e8d7/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg" ],
     address = "100 some rando street",
     isOpenNow = true,
     rating = 4,
@@ -31,7 +42,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     <>
       <RestaurantCard elevation={5}>
         <CardCover key={name} source={{ uri: photos[0] }} />
+        <Info>
         <Title>{name}</Title>
+        <Address>{address}</Address>
+        </Info>
       </RestaurantCard>
     </>
   );
