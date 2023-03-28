196e33555660ba63103c92f9e08e5cd38c1e6873
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2023-03-28 14:30:27 -0700

    card

--- src/components/restaurant-info-card.component.js
+++ src/components/restaurant-info-card.component.js
@@ -1,43 +1,58 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Text, StyleSheet } from "react-native";
-import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-import {SvgXml} from "react-native-svg";
+import { Text, Image, View } from "react-native";
+import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
 import star from "../../assets/star";
+import open from "../../assets/open";
 //styles
 const Title = styled.Text`
-  
   color: ${(props) => props.theme.colors.ui.primary};
   font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body}
+  font-size: ${(props) => props.theme.fontSizes.body};
 `;
 const Address = styled.Text`
-  
   color: ${(props) => props.theme.colors.ui.primary};
   font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption}
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 const RestaurantCard = styled(Card)`
-  background-color: ${(props)=> props.theme.colors.bg.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const CardCover = styled(Card.Cover)`
-  padding: ${(props)=> props.theme.space[3]};
-  background-color: ${(props)=> props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 const Info = styled.View`
-  padding: ${(props)=> props.theme.space[3]};
-`
+  padding: ${(props) => props.theme.space[3]};
+`;
 const Rating = styled.View`
   flex-direction: row;
-  padding-top: ${(props)=> props.theme.space[2]};
-  padding-bottom: ${(props)=> props.theme.space[2]};
-`
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+const Open = styled(SvgXml)`
+  flex-direction: row;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant!",
-    icon,
-    photos = ["https://tb-static.uber.com/prod/image-proc/processed_images/3e2e785f12c20cc3fc37435b2999e8d7/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg" ],
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://tb-static.uber.com/prod/image-proc/processed_images/3e2e785f12c20cc3fc37435b2999e8d7/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg",
+    ],
     address = "100 some rando street",
     isOpenNow = true,
     rating = 4,
@@ -50,13 +65,27 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
       <RestaurantCard elevation={5}>
         <CardCover key={name} source={{ uri: photos[0] }} />
         <Info>
-        <Title>{name}</Title>
-        <Rating>
-        {ratingArray.map(()=> (
-          <SvgXml xml={star}  width={20} height={20}/>
-        ))}
-        </Rating>
-        <Address>{address}</Address>
+          <Title>{name}</Title>
+          <Section>
+            <Rating>
+              {ratingArray.map(() => (
+                <SvgXml xml={star} width={20} height={20} />
+              ))}
+            </Rating>
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <View style={{ paddingLeft: 16 }} />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <View style={{ paddingLeft: 16 }} />
+            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            
+            </SectionEnd>
+          </Section>
+          <Address>{address}</Address>
         </Info>
       </RestaurantCard>
     </>
